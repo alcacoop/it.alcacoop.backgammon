bfea26d432709de29176b7d2065a43a35d4ac15d
Domenico Martella <domenico.martella@alcacoop.it>
2012-11-02 18:56:21 +0100

    INITIAL IMPORT

---
+++ GnuBackgammon-html/src/it/alcacoop/gnubackgammon/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package it.alcacoop.gnubackgammon.client;
+
+import it.alcacoop.gnubackgammon.GnuBakgammon;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new GnuBakgammon();
+	}
+}
