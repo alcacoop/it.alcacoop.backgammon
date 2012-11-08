c582709828031eb1878d8c9cac31323617bf0955
Domenico Martella <domenico.martella@alcacoop.it>
2012-11-08 21:59:00 +0100

    REFACTORING: AICalls has static AIThread and automagically submit AIrunnables

--- GnuBackgammon/src/it/alcacoop/gnubackgammon/logic/AICalls.java
+++ GnuBackgammon/src/it/alcacoop/gnubackgammon/logic/AICalls.java
@@ -1,57 +1,58 @@
 package it.alcacoop.gnubackgammon.logic;
 
-
 import it.alcacoop.gnubackgammon.aicalls.*;
 
 
-
 public class AICalls {
-
-  public static Runnable InitializaEnvironment(String str) {
-    return new InitializeEnvironmentAICall(str);
-  }
-
-  public static Runnable SetAILevel(int l) {
-    return new SetAILevelAICall(l);
-  }
   
-  public static Runnable RollDice(int d[]) {
-    return new RollDiceAICall(d);
-  }
+  static AIThread thread = new AIThread();
   
-  public static Runnable SetBoard(int b1[], int b2[]) {
-    return new SetBoardAICall(b1, b2);
-  }
-  
-  public static Runnable AcceptResign(int r) {
-    return new AcceptResignAICall(r);
+  static {
+    thread = new AIThread();
   }
 
-  public static Runnable AcceptDouble() {
-    return new AcceptDoubleAICall();
+  public static void SetAILevel(int l) {
+    thread.post(new SetAILevelAICall(l));
   }
   
-  public static Runnable UpdateMSCubeInfo(int c, int f) {
-    return new UpdateMSCubeInfoAICall(c, f);
+  public static void RollDice() {
+    thread.post(new RollDiceAICall());
   }
   
-  public static Runnable AskForResignation() {
-    return new AskForResignationAICall();
+  public static void SetBoard(int b1[], int b2[]) {
+    thread.post(new SetBoardAICall(b1, b2));
+  }
+  
+  public static void AcceptResign(int r) {
+    thread.post(new AcceptResignAICall(r));
   }
 
-  public static Runnable AskForDoubling() {
-    return new AskForDoublingAICall();
+  public static void AcceptDouble() {
+    thread.post(new AcceptDoubleAICall());
+  }
+  
+  public static void UpdateMSCubeInfo(int c, int f) {
+    thread.post(new UpdateMSCubeInfoAICall(c, f));
+  }
+  
+  public static void AskForResignation() {
+    thread.post(new AskForResignationAICall());
   }
 
-  public static Runnable EvaluateBestMove(int dices[], int moves[]) {
-    return new EvaluateBestMoveAICall(dices, moves);
+  public static void AskForDoubling() {
+    thread.post(new AskForDoublingAICall());
+  }
+
+  public static void EvaluateBestMove(int dices[], int moves[]) {
+    thread.post(new EvaluateBestMoveAICall(dices));
   }
   
-  public static Runnable SetMatchScore(int AIScore, int HumanScore) {
-    return new SetMatchScoreAICall(AIScore, HumanScore);
+  public static void SetMatchScore(int AIScore, int HumanScore) {
+    thread.post(new SetMatchScoreAICall(AIScore, HumanScore));
   }
   
-  public static Runnable SetGameTurn(int fTurn, int fMove) {
-    return new SetGameTurnAICall(fTurn, fMove);
+  public static void SetGameTurn(int fTurn, int fMove) {
+    thread.post(new SetGameTurnAICall(fTurn, fMove));
   }
+  
 }
