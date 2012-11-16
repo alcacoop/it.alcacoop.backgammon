5baf7359e68fd5871b0d3d2dc622d68c0d8f7a74
Domenico Martella <domenico.martella@alcacoop.it>
2012-11-16 12:39:24 +0100

    REFACTORING: dices.last now is a ArrayList

--- GnuBackgammon/src/it/alcacoop/gnubackgammon/actors/Dices.java
+++ GnuBackgammon/src/it/alcacoop/gnubackgammon/actors/Dices.java
@@ -1,6 +1,8 @@
 package it.alcacoop.gnubackgammon.actors;
 
 
+import java.util.ArrayList;
+
 import it.alcacoop.gnubackgammon.GnuBackgammon;
 import it.alcacoop.gnubackgammon.layers.Board;
 import it.alcacoop.gnubackgammon.logic.MatchState;
@@ -14,10 +16,11 @@ public class Dices extends Group {
 
   private Image d[][];
   private float x, y;
-  private int last[];
+  private ArrayList<Integer> last;
   private Board b;
   
   public Dices(Board _b, float _x, float _y) {
+    last = new ArrayList<Integer>();
     x = _x;
     y = _y-28;
     b = _b;
@@ -45,28 +48,27 @@ public class Dices extends Group {
 
   
   public void show(int d1, int d2) {
+    last.clear();
     if (d1!=d2) {
-      last = new int[2];
-      last[0] = d1;
-      last[1] = d2;
+      last.add(d1);
+      last.add(d2);
     } else {
-      last = new int[4];
       for (int i=0; i<4; i++)
-        last[i] = d1;
+        last.add(d1);
     }
     _show(last);
   }
   
-  private void _show(int _d[]) {
+  private void _show(ArrayList<Integer> _d) {
     hide();
     float x = 0;
-    if (MatchState.fMove==0) x=this.x-60*(_d.length/2)+216;
-    else x=this.x-60*(_d.length/2)-210;
+    if (MatchState.fMove==0) x=this.x-60*(_d.size()/2)+216;
+    else x=this.x-60*(_d.size()/2)-210;
     
-    for (int i=0; i< _d.length; i++) {
-      d[i][_d[i]-1].setX(x+60*i);
-      d[i][_d[i]-1].setY(y);
-      d[i][_d[i]-1].setVisible(true);
+    for (int i=0; i< _d.size(); i++) {
+      d[i][_d.get(i)-1].setX(x+60*i);
+      d[i][_d.get(i)-1].setY(y);
+      d[i][_d.get(i)-1].setVisible(true);
     }
   }
 
@@ -76,20 +78,20 @@ public class Dices extends Group {
     if(b.bearingOff()>=0) {
       int max_dice = 0;
       int is_present = 0;
-      for(int i=0;i<last.length;i++) {
-        if(last[i]>=max_dice)
-          max_dice = last[i];
-        if(last[i]==nn)
+      for(int i=0;i<last.size();i++) {
+        if(last.get(i)>=max_dice)
+          max_dice = last.get(i);
+        if(last.get(i)==nn)
           is_present++;
       }
       
       if(is_present==0)
         n = max_dice;
-    } 
+    }
     
-    if (last.length == 2) {
-      for (int i=0;i<last.length; i++) {
-        if (last[i]==n) { 
+    if (last.size() == 2) {
+      for (int i=0;i<last.size(); i++) {
+        if (last.get(i)==n) { 
           d[i][n-1].setColor(0.7f,0.7f,0.7f,0.4f);
           return;
         }
@@ -105,20 +107,17 @@ public class Dices extends Group {
     }
   }
   
-  public int[] get() {
-    return last;
-  }
   
+  public int[] get() {
+    int ret[] = new int[last.size()];
+    for (int i=0;i<last.size();i++)
+      ret[i] = last.get(i);
+    return ret;
+  }
+
+  //REMOVE A NON PLAYABLE DIXC
   public void remove(int n) {
     disable(n);
-    int t[] = new int[last.length-1];
-    int j = 0;
-    for(int i=0;i<last.length;i++) {
-      if(last[i]!=n) {
-        t[j] = last[i];
-        j++;
-      }
-    }
-    last = t;
+    last.remove(last.indexOf(n));
   }
 }
