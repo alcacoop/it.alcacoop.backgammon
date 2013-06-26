alcacoop/it.alcacoop.backgammon

commit 6b4e13fa79d6533ec5977a36e12b0cdf8c0684c5

FIX: remove ads if user is premium, on menuScreen show (as hide button)

Backgammon-android/src/it/alcacoop/backgammon/MainActivity.java
@@ -694,11 +694,22 @@ public class MainActivity extends AndroidApplication implements NativeFunctions,
     Gdx.graphics.requestRendering();
     if (requestCode == PurchaseActivity.RC_REQUEST) {
       if (isProVersion()) {
-        adView.setVisibility(View.GONE);
-        t.cancel();
+        removeAds();
         GnuBackgammon.Instance.menuScreen.redraw();
       }
     }
+  }
+
+
+  @Override
+  public void removeAds() {
+    runOnUiThread(new Runnable() {
+      @Override
+      public void run() {
+        adView.setVisibility(View.GONE);
+        t.cancel();
+      }
+    });
   }
   
 }

GnuBackgammon-desktop/src/it/alcacoop/backgammon/Main.java
@@ -216,4 +216,10 @@ public class Main implements NativeFunctions {
 
   @Override
   public void inAppBilling() {}
+
+  @Override
+  public void removeAds() {
+    // TODO Auto-generated method stub
+    
+  }
 }

GnuBackgammon/src/it/alcacoop/backgammon/NativeFunctions.java
@@ -52,4 +52,5 @@ public interface NativeFunctions {
   
   public boolean isProVersion();
   public void inAppBilling();
+  public void removeAds();
 }

GnuBackgammon/src/it/alcacoop/backgammon/layers/MainMenuScreen.java
@@ -169,6 +169,7 @@ public class MainMenuScreen extends BaseScreen {
       table.row().pad(1);
       table.add(rate).expand().fill().width(table.getWidth()/2);
       table.add(about).expand().fill().width(table.getWidth()/2);
+      GnuBackgammon.Instance.nativeFunctions.removeAds();
     } else {
       table.row().pad(1);
       table.add(howtoplay).expand().fill().width(table.getWidth()/2);

commit 23f8bdd8e119cdeb5246f7ea5e038791958325ac

FIX: regression from 6b4e13f

Backgammon-android/src/it/alcacoop/backgammon/MainActivity.java
@@ -280,6 +280,7 @@ public class MainActivity extends AndroidApplication implements NativeFunctions,
 
   @Override
   public void showAds(boolean show) {
+    if (isProVersion()) return;
     handler.sendEmptyMessage(show ? SHOW_ADS : HIDE_ADS);
   }
 
@@ -626,6 +627,7 @@ public class MainActivity extends AndroidApplication implements NativeFunctions,
 
   @Override
   public void showInterstitial() {
+    if (isProVersion()) return;
     runOnUiThread(new Runnable() {
       @Override
       public void run() {
@@ -694,22 +696,12 @@ public class MainActivity extends AndroidApplication implements NativeFunctions,
     Gdx.graphics.requestRendering();
     if (requestCode == PurchaseActivity.RC_REQUEST) {
       if (isProVersion()) {
-        removeAds();
+        adView.setVisibility(View.GONE);
+        t.cancel();
         GnuBackgammon.Instance.menuScreen.redraw();
       }
     }
   }
 
-
-  @Override
-  public void removeAds() {
-    runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        adView.setVisibility(View.GONE);
-        t.cancel();
-      }
-    });
-  }
   
 }

GnuBackgammon-desktop/src/it/alcacoop/backgammon/Main.java
@@ -216,10 +216,4 @@ public class Main implements NativeFunctions {
 
   @Override
   public void inAppBilling() {}
-
-  @Override
-  public void removeAds() {
-    // TODO Auto-generated method stub
-    
-  }
 }

GnuBackgammon/src/it/alcacoop/backgammon/NativeFunctions.java
@@ -52,5 +52,4 @@ public interface NativeFunctions {
   
   public boolean isProVersion();
   public void inAppBilling();
-  public void removeAds();
 }

GnuBackgammon/src/it/alcacoop/backgammon/layers/MainMenuScreen.java
@@ -169,7 +169,6 @@ public class MainMenuScreen extends BaseScreen {
       table.row().pad(1);
       table.add(rate).expand().fill().width(table.getWidth()/2);
       table.add(about).expand().fill().width(table.getWidth()/2);
-      GnuBackgammon.Instance.nativeFunctions.removeAds();
     } else {
       table.row().pad(1);
       table.add(howtoplay).expand().fill().width(table.getWidth()/2);